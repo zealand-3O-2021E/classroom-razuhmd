b8cc001800db5a4e1c1956a053199e44170c36bc
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-09-06 22:18:04 +0200

    Task 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthDay;
+
+        public Student(string name, int birthMonth, int birthDay)
+        {
+            _name = name;
+            _birthMonth = birthMonth;
+            _birthDay = birthDay;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public int BirthMonth
+        {
+            get { return _birthMonth; }
+        }
+        public int BirthDay
+        {
+            get { return _birthDay; }
+        }
+    }
+}
